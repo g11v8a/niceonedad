a8ebdef2a0a80badd3cb819b8f7444e174486112
G <12945959+g11v8a@users.noreply.github.com>
2019-03-28 09:17:38 +0800

    Update command description

--- src/Commands/JokeCommand.php
+++ src/Commands/JokeCommand.php
@@ -19,7 +19,7 @@ class JokeCommand extends Command
      *
      * @var string
      */
-    protected $description = 'Random (bad) joke generator.';
+    protected $description = 'Random dad joke generator.';
 
     /**
      * Create a new command instance.
